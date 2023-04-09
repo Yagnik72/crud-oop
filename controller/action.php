ffccd5c6a53fd9b834d3480c43d1fcb3bdc25ee3
Yagnik72 <yagnikpadaliya72@gmail.com>
2023-04-09 20:05:57 +0530

    initial commit

---
+++ controller/action.php
@@ -0,0 +1,6 @@
+<?php
+
+
+if(isset($_POST['action']) && $_POST['action'] == 'view'){
+  
+}
---
+++ model/db.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace model\Database;
+
+class Database {
+
+    private $dns = 'mysql:host=localhost;dbname=crud_oops';
+    private $user = 'root';
+    private $pass = '';
+    public $conn;
+
+    public function __construct() {
+        try {
+            $this->conn = new PDO( $this->dns, $this->user, $this->pass );
+        } catch ( PDOException $e ) {
+            echo $e->getMessage();
+        }
+    }
+
+    public function insert( $fname, $lname, $email, $phone ) {
+        $sql = 'INSERT INTO users (first_name, last_name, email, phone) VALUES (:fname, :lname, :email, :phone)';
+        $stmt = $this->conn->prepare( $sql );
+        $stmt->execute( [ 'fname'=>$fname, 'lname'=>$lname, 'email'=>$email, 'phone'=>$phone ] );
+        return true;
+    }
+
+    public function read() {
+        $data = array();
+        $sql = 'SELECT * FROM users';
+        $stmt = $this->conn->prepare( $sql );
+        $stmt->execute();
+        $result = $stmt->fetchAll( PDO::FETCH_ASSOC );
+        foreach ( $result as $row ) {
+            $data[] = $row;
+        }
+        return $data;
+    }
+
+    public function getUserById( $id ) {
+        $sql = 'SELECT * FROM users WHERE id = :id';
+        $stmt = $this->conn->prepare( $sql );
+        $stmt->execute( [ 'id'=>$id ] );
+        $result = $stmt->fetch( PDO::FETCH_ASSOC );
+        return $result;
+    }
+
+    public function update( $id, $fname, $lname, $email, $phone ) {
+        $sql = 'UPDATE users SET first_name=:fname, last_name=:lname, email=:email, phone=phone WHERE id = :id';
+        $stmt = $this->conn->prepare( $sql );
+        $stmt->execute( [ 'fname'=>$fname, 'lname'=>$lname, 'email'=>$email, 'phone'=>$phone, 'id'=>$id ] );
+        return true;
+    }
+
+    public function delete( $id ) {
+        $sql = 'DELETE FROM users WHERE id = :id';
+        $stmt = $this->conn->prepare( $sql );
+        $stmt->execute( [ 'id'=>$id ] );
+        return true;
+    }
+
+    public function totalRowCount() {
+        $sql = 'SELECT * FROM users';
+        $stmt = $this->conn->prepare( $sql );
+        $stmt->execute();
+        $t_row = $stmt->rowCount();
+
+        return $t_row;
+    }
+
+}
+
